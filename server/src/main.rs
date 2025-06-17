01b5858bfa44d0a46ccc9280b4e612ae0d322f6b
compusophy <79760496+compusophy@users.noreply.github.com>
2025-06-16 21:02:43 -0600

    fix(server): add CorePlugin to initialize Bevy task system

--- server/src/main.rs
+++ server/src/main.rs
@@ -80,7 +80,7 @@ async fn main() {
 
     let bevy_thread = std::thread::spawn(move || {
         let mut app = App::new();
-        app.add_plugins(MinimalPlugins);
+        app.add_plugins((MinimalPlugins, bevy::core::CorePlugin));
         app.insert_resource(Clients::default());
         app.insert_resource(WorldCommandRx(world_cmd_rx));
         app.insert_resource(ClientMessageRx(client_msg_rx));
